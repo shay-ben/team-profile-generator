605464bef1d6d313a1f989a84ad896ca950fd093
Your Name <you@example.com>
2022-06-08 03:36:50 -0400

    employee file setup

---
+++ Develop/lib/employee.js
@@ -0,0 +1,26 @@
+class employee {
+  constructor(name, id, email, position) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.position = position;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getID() {
+    return this.name;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getPosition() {
+    return this.position;
+  }
+}
+
+module.exports = employee;
---
+++ Develop/test/employee.test.js
@@ -0,0 +1,12 @@
+const employee = require("./Deveope/lib/employee");
+
+const employee = new employee();
+
+test("test if you can find a constructor for the object", ()
+    expect(employee.name).toBe(jared )
+    expect(employee.ID).toBe(1 )
+    expect(employee.email).toBe 'fakejared@gmal.com" 
+
+
+
+);
