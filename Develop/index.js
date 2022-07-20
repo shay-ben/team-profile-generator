ff4035def676ba8aebc637b1836bb18a5ae88522
Your Name <you@example.com>
2022-07-20 16:45:11 -0400

    required files

---
+++ Develop/index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const employee = require('./lib/employee');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+const manager = require('./lib/manager');
+
+const team = []; 
+
+const inquirer = () => { 
+    return inquirer
+        .prompt([
+            {
+
+
+                
+            }
+
+
+
+        ])
+
+
+}
